2a357914c8df924073098af66bdefe9471f5316d
MinSeok Oh <minseok.dev@gmail.com>
2025-01-02 00:34:03 +0900

    fix: Customer 이름 대문자로 변경

---
+++ accessing-relational-data-using-jdbc-with-spring/src/test/java/com/java/study/domain/Customer.java
@@ -0,0 +1,45 @@
+package com.java.study.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("CUSTOMER")
+public class Customer {
+
+    @Id
+    private Long id;
+
+    @Column("FIRSTNAME")
+    private String firstName;
+
+    @Column("LASTNAME")
+    private String lastName;
+
+    public Customer(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
