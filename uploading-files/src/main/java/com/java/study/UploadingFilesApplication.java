a96a07e094342ca43d2b52a93dc8e66403931c3f
MinSeok Oh <minseok.dev@gmail.com>
2025-01-02 14:16:45 +0900

    feat: 프로젝트 요구사항 구현

--- uploading-files/src/main/java/com/java/study/UploadingFilesApplication.java
+++ uploading-files/src/main/java/com/java/study/UploadingFilesApplication.java
@@ -1,7 +1,11 @@
 package com.java.study;
 
+import com.java.study.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UploadingFilesApplication {
@@ -10,4 +14,11 @@ public class UploadingFilesApplication {
         SpringApplication.run(UploadingFilesApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
