084b169235fa48bcb0e91fd9da5777c7cf460384
MinSeok Oh <minseok.dev@gmail.com>
2025-01-03 11:35:52 +0900

    setting: 기본 프로젝트 세팅

---
+++ authenticating-a-user-with-ldap/src/main/java/com/java/study/AuthenticatingAUserWithLdapApplication.java
@@ -0,0 +1,13 @@
+package com.java.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingAUserWithLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticatingAUserWithLdapApplication.class, args);
+    }
+
+}
