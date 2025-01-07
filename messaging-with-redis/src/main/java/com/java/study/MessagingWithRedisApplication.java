d4443a777dcda3f0201db6916a54a623f0479233
MinSeok Oh <minseok.dev@gmail.com>
2025-01-08 00:07:38 +0900

    setting: 기본 프로젝트 생성

---
+++ messaging-with-redis/src/main/java/com/java/study/MessagingWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.java.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWithRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingWithRedisApplication.class, args);
+    }
+
+}
